34c0d27573902da6022ff585aa9ddb75de89efd2
Furkan Hamza Bolat <70775462+xNart06@users.noreply.github.com>
2022-10-29 13:58:37 +0300

    Add files via upload

--- StackWithArray.c
+++ StackWithArray.c
@@ -5,27 +5,27 @@
 //pop() = cikarma
 //peek() = en bastaki degeri dondurme
 
-int dizi[Boyut], top;
+int dizi[Boyut], top; //top main icinde -1 olarak tanimlandi
 
 void push(int veri){
-    if(top+1 == Boyut){
+    if(top+1 == Boyut){ // top -1 oldugu icin 1 arttirdik ve bu deger boyuta esitse stack dolu hatasi verecek
         printf("stack dolu");
     }else{
-        top = top + 1;
-        dizi[top] = veri;
+        top = top + 1; 
+        dizi[top] = veri; //degilse her seferinde index numarasini 1 arttiracak ve dizideki ilgili alana bu yazacak
     }
 }
 
 void pop(){
-    if(top == -1){
+    if(top == -1){ //stack ilk degeri olan -1 e esitse yani bossa bu stacgin bos oldugu anlamina gelir
         printf("stack zaten bos");
     }else{
-        top = top-1;
+        top = top-1; //eger degilse son eklenen degeri stackten siler
     }
 }
 
 int peek(){
-    return dizi[top];
+    return dizi[top]; //son elemani gormek icin diziyi geri donduruyoruz
 }
 
 void yazdir(){
@@ -58,7 +58,7 @@ int main(){
                 yazdir();
                 break;
             case 3:
-               printf("\n Bagli listenin top pozisyonundaki elemani = %d", peek());
+                printf("\n Bagli listenin top pozisyonundaki elemani = %d", peek());
                 break;
         }
     }
